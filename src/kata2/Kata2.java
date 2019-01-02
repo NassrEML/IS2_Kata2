df2a10fc669db118f4712c943656e73f7186a7e8
NassrEML <NassrEML@DESKTOP-JK3N8UH>
2019-01-02 19:03:06 +0100

    Tercera version de Kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,6 +1,5 @@
 package kata2;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -10,14 +9,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Map<Integer, Integer> histogram = new HashMap<>();
-        Integer[] data = new Integer[] {1,2,3,4,5,6,7,8,9,5,3,2,1,2,6};
 
-        for (Integer key: data){
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key)+1 : 1);
-        }
+        Integer[] data = new Integer[] {1,2,3,4,5,6,7,8,9,5,3,2,1,2,6};
+        Histogram histo = new Histogram(data);
+        Map<Integer, Integer> histogr = histo.getHistogram();
         
-        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : histogr.entrySet()) {
             Integer key = entry.getKey();
             Integer value = entry.getValue();
             System.out.println(key + " ==> " + value);
